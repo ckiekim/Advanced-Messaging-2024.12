fbb5d0be924166b8b66f9fbb1bb570294e0fdbd4
Cheolkie Kim <ckiekim@naver.com>
2025-01-08 17:18:12 +0900

    ApexCharts

--- src/main/java/com/lion/ws/controller/StockController.java
+++ src/main/java/com/lion/ws/controller/StockController.java
@@ -3,27 +3,30 @@ package com.lion.ws.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
 @Controller
-@RequestMapping("/crypto")
-public class CryptoController {
+@RequestMapping("/stock")
+public class StockController {
     @Value("${polygon.io.key}") private String apiKey;
 
     @GetMapping("/chart")
     public String chart() {
-        return "crypto/chart";
+        return "stock/chart";
     }
 
-    @GetMapping("/msft")
+    @GetMapping("/apex")
+    public String apex() {
+        return "stock/apex";
+    }
+
+    @GetMapping("/ticker/{ticker}")
     @ResponseBody
-    public Map<String, Object> getBitcoinData() {
-        String url = "https://api.polygon.io/v2/aggs/ticker/MSFT/range/1/day/2024-01-01/2024-03-31?apiKey=" + apiKey;
+    public Map<String, Object> getCandleData(@PathVariable String ticker, @RequestParam String startDay, @RequestParam String endDay) {
+        String url = "https://api.polygon.io/v2/aggs/ticker/" + ticker + "/range/1/day/" + startDay + "/" + endDay + "?apiKey=" + apiKey;
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<Map> response = restTemplate.getForEntity(url, Map.class);
         return response.getBody();
